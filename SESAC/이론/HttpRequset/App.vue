fe0eb62b8ce3100a9af5a67ca24fd00a9f8073a5
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-16 17:24:28 +0900

    Create App.vue

---
+++ SESAC/이론/HttpRequset/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Hello HTTP Request</h2>
+    <TodoList />
+  </div>
+</template>
+<script>
+import TodoList from "./components/http/TodoList";
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>

adc7cb0c065c32b696856e32a5a79d4bbe390310
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-16 13:36:54 +0900

    Create DetailView.vue

---
+++ SESAC/이론/컴포넌트/components4/DetailView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <h2>Detail Page</h2>
+      <button @click="closeDetail">close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "detailPage",
+  methods: {
+    closeDetail() {
+      this.$emit("closeDetail");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  background: #fff;
+  width: 60%;
+}
+</style>

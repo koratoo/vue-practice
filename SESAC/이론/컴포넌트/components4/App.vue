b05f570a2f01c81c5229b867653e42fcf5af5a71
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-16 13:37:21 +0900

    Create App.vue

---
+++ SESAC/이론/컴포넌트/components4/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Hello Component</h2>
+    <button @click="displayDetail = true">show detail</button>
+    <DetailView v-if="displayDetail" @closeDetail="close" />
+  </div>
+</template>
+
+<script>
+import DetailView from "./components/DetailView.vue";
+export default {
+  name: "App",
+  components: {
+    DetailView,
+  },
+  data() {
+    return {
+      displayDetail: false,
+    };
+  },
+  methods: {
+    close() {
+      this.displayDetail = false;
+      console.log("event emit!!!");
+    },
+  },
+};
+</script>
+
+<style></style>

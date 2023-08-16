7c5c5daf9c084291434e2c96bbac348c3a9b1640
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-16 15:59:00 +0900

    Create App.vue

---
+++ SESAC/이론/슬롯/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Hello Slots</h2>
+    <CardView>
+      <template v-slot:header>
+        <h3>RandomImage</h3>
+      </template>
+      <template v-slot:body>
+        <img src="https://picsum.photos/200/200" />
+      </template>
+      <template v-slot:footer>
+        <small>thank you!</small>
+      </template>
+    </CardView>
+  </div>
+</template>
+<script>
+import CardView from "./components/slots/CardView";
+export default {
+  name: "App",
+  components: {
+    CardView,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>

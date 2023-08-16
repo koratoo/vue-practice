952ca69b9b38c9a06a6afe0b817036c20a75a5a0
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-16 11:11:12 +0900

    Create App.vue

---
+++ SESAC/이론/컴포넌트/components3/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ProductList :products="products" />
+  </div>
+</template>
+
+<script>
+import ProductList from "./components/ProductList";
+export default {
+  name: "App",
+  components: {
+    ProductList,
+  },
+  data() {
+    return {
+      products: [
+        { id: 0, name: "TV", price: 500000, company: "LG" },
+        { id: 1, name: "전자렌지", price: 200000, company: "삼성" },
+        { id: 2, name: "가스오븐", price: 300000, company: "한화" },
+        { id: 3, name: "냉장고", price: 50000, company: "대우" },
+        { id: 4, name: "에어컨", price: 1000000, company: "해태" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>

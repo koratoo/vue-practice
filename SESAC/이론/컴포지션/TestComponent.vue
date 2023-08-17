730ee69ba336bc016aaf2f5713189531edda3dfd
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-17 16:38:25 +0900

    Create TestComponent.vue

---
+++ SESAC/이론/컴포지션/TestComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>handsome? {{ isHandsome }}</h2>
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from "vue";
+export default {
+  name: "TestComponent",
+  setup() {
+    let isHandsome = ref(false);
+    const state = reactive({
+      username: "Scalper",
+      age: 50,
+      job: "programmer",
+    });
+
+    return {
+      isHandsome,
+      ...toRefs(state),
+    };
+  },
+  //   data() {
+  //     return {
+  //       username: "Scalper",
+  //     };
+  //   },
+};
+</script>
+
+<style></style>

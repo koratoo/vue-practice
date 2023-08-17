9041169f3346264164421309e2036165e5190d50
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-17 15:38:48 +0900

    Create FeeStatus.vue

---
+++ SESAC/이론/mixins/FeeStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>합산된 요금 : {{ totalMoney }}</h2>
+    <button @click="addMoney(50)">50</button>
+    <button @click="addMoney(100)">100</button>
+    <button @click="addMoney(200)">200</button>
+    <button @click="addMoney(250)">250</button>
+  </div>
+</template>
+
+<script>
+import moneyMixins from "../../mixins/moneyMixins";
+
+export default {
+  name: "feeStatus",
+  mixins: [moneyMixins],
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>

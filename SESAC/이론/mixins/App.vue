6fa05032505b03109948334b5cc259df6ffa1327
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-17 15:39:07 +0900

    Create App.vue

---
+++ SESAC/이론/mixins/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Hello Mixins</h2>
+    <ProductStatus />
+
+    <FeeStatus />
+    <SavingStatus />
+  </div>
+</template>
+<script>
+import ProductStatus from "./components/mixin/ProductStatus";
+import FeeStatus from "./components/mixin/FeeStatus";
+import SavingStatus from "./components/mixin/SavingStatus";
+export default {
+  name: "App",
+  components: { ProductStatus, SavingStatus, FeeStatus },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>

0b39eee9ba4afc63e8774f045d8a427a6be07df9
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-17 09:52:33 +0900

    Create App.vue

---
+++ SESAC/이론/lifecycle/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Hello Life Cycle</h2>
+    <ParentComp />
+  </div>
+</template>
+<script>
+import ParentComp from "./components/lifecycle/ParentComp";
+export default {
+  name: "App",
+  components: { ParentComp },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>

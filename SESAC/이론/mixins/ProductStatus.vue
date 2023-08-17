de35aba450514e570d74c1a448b5d373aee3f445
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-17 15:37:42 +0900

    Create ProductStatus.vue

---
+++ SESAC/이론/mixins/ProductStatus.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>전체금액 : {{ totalMoney }}</h2>
+    <div>
+      <button @click="addMoney(100)">100</button>
+      <button @click="addMoney(200)">200</button>
+      <button @click="addMoney(300)">300</button>
+      <button @click="addMoney(400)">400</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import moneyMixins from "../../mixins/moneyMixins";
+export default {
+  name: "productStatus",
+  mixins: [moneyMixins],
+  components: {},
+  data() {
+    return {
+      totalMoney: 3000,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style></style>

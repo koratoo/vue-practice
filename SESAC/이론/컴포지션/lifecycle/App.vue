d6bfe323b46c3496c585fd8055c3fb169e246bec
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-18 10:53:03 +0900

    Create App.vue

---
+++ SESAC/이론/컴포지션/lifecycle/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Hello Composition API</h2>
+    <TestComponent />
+  </div>
+</template>
+<script>
+import TestComponent from "./components/composition/TestComponent";
+export default {
+  name: "App",
+  components: { TestComponent },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>

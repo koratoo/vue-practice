e967b5475be91b8f3542456557fc91d5985b55a3
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-18 11:15:17 +0900

    Create FeeStatus.vue

---
+++ SESAC/이론/컴포지션/reusability/FeeStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>합산된 요금 : {{ totalMoney }}</h2>
+    <button @click="addMoney(50)">50</button>
+    <button @click="addMoney(100)">100</button>
+    <button @click="addMoney(200)">200</button>
+    <button @click="addMoney(250)">250</button>
+  </div>
+</template>
+
+<script>
+import useMoney from "../../composables/useMoney";
+export default {
+  name: "feeStatus",
+  setup() {
+    const { addMoney, totalMoney } = useMoney();
+
+    return {
+      addMoney,
+      totalMoney,
+    };
+  },
+};
+</script>
+
+<style></style>

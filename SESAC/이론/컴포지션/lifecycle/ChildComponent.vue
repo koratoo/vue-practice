89b3f7d606f37bc1cbdbf88b04610241ce9111e8
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-18 10:52:09 +0900

    Create ChildComponent.vue

---
+++ SESAC/이론/컴포지션/lifecycle/ChildComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>{{ fullname }}</h3>
+    <button @click="sendParent">child click</button>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "childComponent",
+  props: ["firstname", "lastname"],
+  setup(props, context) {
+    console.log(context);
+    const fullname = computed(() => {
+      return `${props.firstname} ${props.lastname}`;
+    });
+    function sendParent() {
+      context.emit("sendParent");
+    }
+    return {
+      fullname,
+      sendParent,
+    };
+  },
+};
+</script>
+
+<style></style>

7ce0b48007c5a1c9878cded46a924a93385202b5
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-18 10:52:33 +0900

    Create TestComponent.vue

---
+++ SESAC/이론/컴포지션/lifecycle/TestComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Hello LifeCycle</h2>
+    <ChildComponent
+      firstname="Code"
+      lastname="Scalper"
+      @sendParent="sendParent"
+    />
+  </div>
+</template>
+
+<script>
+import ChildComponent from "./ChildComponent.vue";
+import {} from "vue";
+export default {
+  name: "TestComponent",
+  components: { ChildComponent },
+  methods: {
+    sendParent() {
+      console.log("parent event!!!");
+    },
+  },
+};
+</script>
+
+<style></style>

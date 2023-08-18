223803bd13665cd688b1ce28a7b32bf6950396af
jskim <96603612+koratoo@users.noreply.github.com>
2023-08-18 11:14:54 +0900

    Create SavingStatus.vue

---
+++ SESAC/이론/컴포지션/reusability/SavingStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>총 적금 : {{ totalMoney }}</h2>
+    <button @click="addMoney(1000)">1000</button>
+    <button @click="addMoney(2000)">2000</button>
+    <button @click="addMoney(1150)">1150</button>
+    <button @click="addMoney(2200)">2200</button>
+  </div>
+</template>
+
+<script>
+import useMoney from "../../composables/useMoney";
+export default {
+  name: "savingStatus",
+  setup() {
+    const { addMoney, totalMoney } = useMoney(5000);
+
+    return {
+      addMoney,
+      totalMoney,
+    };
+  },
+};
+</script>
+
+<style></style>
